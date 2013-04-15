0f9e0f211d4ab2502b1d6f23a27c687636b80766
Tim Galloway <Tim.Galloway@leicon.com.au>
2013-04-15 14:58:14 +0800

    Added

---
+++ GPSApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("GPSApp")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Leighton Contractors")> 
+<Assembly: AssemblyProduct("GPSApp")> 
+<Assembly: AssemblyCopyright("Copyright © Leighton Contractors 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a7362755-8cff-4339-ae1a-904202a6443a")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
